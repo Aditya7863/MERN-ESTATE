42853b8f71c9c4618d18a185d19b1885bf9c3de3
Aditya7863 <119505076+Aditya7863@users.noreply.github.com>
2024-07-09 19:42:00 +0530

    connected to mongoDB

--- api/index.js
+++ api/index.js
@@ -1,7 +1,11 @@
 import express from 'express';
+import colors from 'colors';
+import mongoose from 'mongoose';
+import { connectDB } from './db/db.js'; 
 
+connectDB();
 const app = express();
 
 app.listen(process.env.PORT || 5000,()=>{
-    console.log(`Listening on port number ${process.env.PORT}`)
-})
+    console.log(`Listening on port number ${process.env.PORT}`);
+});
